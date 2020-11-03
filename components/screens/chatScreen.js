8495045c9d1f81f2773ef9ae64dfd9817eaca304
fairnia <hello@fairnia.com>
2020-11-03 12:35:11 -0800

    greeting run through

--- components/screens/chatScreen.js
+++ components/screens/chatScreen.js
@@ -4,38 +4,44 @@ import Header from '../header'
 import Footer from '../footer'
 
 const greetings = [
-  {'greeting': 'Say hello to your partner' ,"duration":700},
-  {'greeting': 'May you be happy' ,"duration":700},
-  {'greeting': 'May you you be healthy' ,"duration":700},
-  {'greeting': 'May you stay safe' ,"duration":700},
-  {'greeting': 'Share a moment of presence with your partner' ,"duration":700},
+  {'greeting': 'Say hello to your partner' ,"duration":200},
+  {'greeting': 'May you be happy' ,"duration":200},
+  {'greeting': 'May you you be healthy' ,"duration":4000},
+  {'greeting': 'May you stay safe' ,"duration":500},
+  {'greeting': 'Share a moment of presence with your partner' ,"duration":4000},
   {'greeting': 'Thank your partner and say goodbye' ,"duration":700},
+  {'greeting': '' ,"duration":700},
 ]
 
 export default function ChatScreen({navigation}) {
-  const [greeting, setGreeting] = useState(greetings[0].greeting);
+  const [greeting, setGreeting] = useState('');
 
-  var interval = setInterval(function(){
-    var obj = json.objects[i];
-    // do whatever
-    i++;
-    if(i === greetings.length) clearInterval(interval);
-  }, 5000);
-
-  useEffect(() => {
-    var interval = setInterval(function(){
-      var obj = json.objects[i];
-      // do whatever
+  var i = 0;
+  const callGreeting = ()=>{
+    setTimeout(()=>{
+      setGreeting(greetings[i].greeting);
       i++;
-      if(i === greetings.length) clearInterval(interval);
-    }, 5000);
-  });
+      callNextGreeting(i);
+    }, greetings[i].duration);
+  }
+
+  const callNextGreeting = (i) =>{
+    console.log('next greeting called ', i);
+    if( greetings.length > i) {
+      callGreeting();
+    }
+  }
+
 
   return (
     <View style={styles.footer}>
       <Header navigation = {navigation}/>
       <Text>hear ye! hear ye! This is my chatscreen!</Text>
       <Text>{greeting}</Text>
+      <Button
+      title="Start Chatting"
+      onPress={() => callGreeting()}
+      />
       <Button
       title="Stop Chatting"
       onPress={() => navigation.navigate('Thankyou')}
